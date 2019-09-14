9c052f0cc6995f44741a133385945b046650b34e
rodgeraraujo <rogerio.araujo@mail.com>
2019-09-14 02:22:56 -0300

    Páginas de carreiras, e corrigindo urls

--- vagas/urls.py
+++ vagas/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import vagas_list, index
+from .views import vagas_list, index, developer_page,ux_designer_page, marketing_page, project_manager_page, designer_page
 
 urlpatterns = [
     path("", index, name="index"),
-    path("carreiras", vagas_list)
+    path("carreiras", vagas_list),
+    path("carreiras/developer", developer_page),
+    path("carreiras/ux-designer", ux_designer_page),
+    path("carreiras/marketing", marketing_page),
+    path("carreiras/project-manager", project_manager_page),
+    path("carreiras/designer", designer_page)
 ]
 
--- vagas/views.py
+++ vagas/views.py
@@ -9,3 +9,20 @@ def index(request):
 
 def candidatura(request):
     pass
+
+# vagas páginas
+def developer_page(request):
+    return render(request, "vagas/developer_page.html")
+
+def designer_page(request):
+    return render(request, "vagas/designer_page.html")
+
+def ux_designer_page(request):
+    return render(request, "vagas/ux_designer_page.html")
+
+def marketing_page(request):
+    return render(request, "vagas/marketing_page.html")
+
+def project_manager_page(request):
+    return render(request, "vagas/project_manager_page.html")
+
